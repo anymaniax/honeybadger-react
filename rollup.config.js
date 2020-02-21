016a43daa99a5bcd359934dd822bf51f0cd1d09a
Victor Bury <victor@c02wx0kag8wn.lan>
2020-02-21 20:27:35 +0100

    feat(update): babel, rollup &  react

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,45 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
-import path from 'path'
-
-import pkg from './package.json'
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
+import path from "path";
+import babel from "rollup-plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true,
       sourcemapPathTransform: relativePath => {
         // will transform e.g. "src/main.js" -> "main.js"
-        return path.relative('src', relativePath)
-      },
-
+        return path.relative("src", relativePath);
+      }
     },
     {
       file: pkg.module,
-      format: 'es',
+      format: "es",
       sourcemap: true,
       sourcemapPathTransform: relativePath => {
         // will transform e.g. "src/main.js" -> "main.js"
-        return path.relative('src', relativePath)
+        return path.relative("src", relativePath);
       }
     },
     {
       file: pkg.unpkg,
-      format: 'iife',
-      name: 'HoneybadgerReact',
+      format: "iife",
+      name: "HoneybadgerReact",
       sourcemap: true,
       sourcemapPathTransform: relativePath => {
-        return path.relative('src', relativePath)
+        return path.relative("src", relativePath);
       },
       globals: {
-        'react': 'React',
-        'prop-types': 'PropTypes'
+        react: "React",
+        "prop-types": "PropTypes"
       }
     }
   ],
@@ -53,10 +51,9 @@ export default {
     url(),
     svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      exclude: "node_modules/**"
     }),
     resolve(),
     commonjs()
   ]
-}
+};
